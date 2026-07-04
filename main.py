c142105c21439ff87cef2d230690e1df15303381
vityaloza-png <vitya.loza@icloud.com>
2026-07-04 23:11:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,6 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(button_callback))
-    print(“Бот запускається…")
+    print("Бот запускається…")
     app.run_polling()
 
